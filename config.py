bf2242eeaa4cef2f6a2ffbc8e240500a0b424c50
Beaver1111 <tamaca.beaver@gmail.com>
2025-03-25 13:24:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Square Sandbox API Credentials
+SQUARE_ACCESS_TOKEN = ""
+SQUARE_LOCATION_IDS = ["", ]
+
+# Local save directory for testing
+LOCAL_SAVE_DIR = "C:/Users/Administrator/Documents/SquarePOS_Exports"
